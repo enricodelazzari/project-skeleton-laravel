c17bbfa9f300f470e8e6caaa2fca75bb5562043d
Enrico De Lazzari <enricodelazzari@users.noreply.github.com>
2021-11-06 21:00:33 +0100

    FIX sanctum config

--- configure.php
+++ configure.php
@@ -163,6 +163,10 @@ replace_in_file('config/auth.php', [
     'App\Models\User::class' => 'Domain\Users\Models\User::class',
 ]);
 
+replace_in_file('config/sanctum.php', [
+    'App\Http\Middleware' => 'Support\Middleware',
+]);
+
 edit_composer_json(function ($data) {
     $data['autoload']['psr-4'] = [
         'App\\' => 'src/App/',
